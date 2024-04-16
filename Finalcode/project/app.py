4fb1f4fcd1e349a2f840b425ec914c5def5ffb97
anvithagowdaa <71768238+projectwale@users.noreply.github.com>
2024-04-16 16:47:30 +0530

    hi

---
+++ Finalcode/project/app.py
@@ -0,0 +1,461 @@
+from flask import Flask, render_template, request, session, url_for, redirect, jsonify
+import pymysql
+from werkzeug.utils import secure_filename
+import pathlib
+import pandas as pd
+import cv2
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+import json
+
+
+
+
+
+app = Flask(__name__)
+app.secret_key = 'any random string'
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/profile/'
+app.config['UPLOADED_PHOTOS_DEST1'] = 'static/product/'
+app.config['UPLOADED_PHOTOS_DEST2'] = 'static/skindetecttype'
+
+app.secret_key = 'any random string'
+model = load_model('VGGSKin.hp5')
+
+def dbConnection():
+    connection = pymysql.connect(host="localhost", user="root", password="root", database="032-Cosmetics")
+    return connection
+
+def dbClose():
+    try:
+        dbConnection().close()
+    except:
+        print("Something went wrong in Close DB Connection")
+        
+        
+                
+con = dbConnection()
+cursor = con.cursor()
+
+
+@app.route('/', methods=["GET","POST"])
+def main():
+    
+    return render_template('main.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        Email = request.form.get("Email")
+        Password = request.form.get("password")
+        print(Password)
+        con = dbConnection()
+        cursor = con.cursor()
+        result_count = cursor.execute('SELECT * FROM userregisters WHERE Email = %s AND Password = %s', (Email, Password))
+        print(result_count)
+        
+        if result_count == 1:
+            res = cursor.fetchone()
+            print(res)
+            session['user'] = res[1]
+            session['uid'] = res[0]
+            session['image'] = res[5]
+            # Successful login logic
+            # return jsonify({'message': 'Login successful', 'user_id': res[0]})
+            return "success"
+        else:
+            # Failed login logic
+            # return jsonify({'message': 'Login failed'})
+            return "fail"
+
+        con.close()
+    
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+    
+    return render_template('register.html')
+
+@app.route('/register1',methods=['POST'])
+def register1():
+    
+    print("ouy")
+    if request.method =='POST':
+        print("in record")
+        details = request.form
+      
+        Username = details['Username']
+        email = details['email']
+        Mobile = details['Mobile'] 
+        Password = details['Password']
+        Address = details['Address'] 
+        Pancard = details['Pancard']
+        uploadimg = request.files['file']
+        
+        
+        con = dbConnection()
+        cursor = con.cursor()
+        cursor.execute('SELECT * FROM userregisters WHERE Email = %s', (email))
+        res = cursor.fetchone()
+      
+        
+        filename_secure = secure_filename(uploadimg.filename)
+        uploadimg.save(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename_secure))
+        filenamepath = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename_secure)
+        
+        if not res:
+            sql = "INSERT INTO userregisters(Username, Email, Mobile, Password, Profile_Img, Address, Pancard) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            val = (Username, email, Mobile, Password, filenamepath,Address,Pancard)
+            cursor.execute(sql, val)
+            con.commit()
+          
+    
+            message = "Registration USER successfully added by USER side. Username: " + Username
+            # return redirect(url_for('index'))
+            return jsonify({'message': message})
+            message = "Already available"
+            
+        else:
+            message = "Registration USER not  added by USER side. Username: " + Username
+            dbClose()
+            # return redirect(url_for('index'))
+            return jsonify({'message': message})
+              
+@app.route('/SessionHandle1',methods=['POST','GET'])
+def SessionHandle1():
+    if request.method == "POST":
+        details = request.form
+        name = details['name']
+        session['name'] = name
+        strofuser = name
+        print (strofuser.encode('utf8', 'ignore'))
+        return strofuser      
+
+@app.route('/adminlogin')
+def adminlogin():
+    return render_template('adminlogin.html')
+
+
+@app.route('/index')
+def index():
+    username=session['user']
+    img=session['image']
+    con = dbConnection()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM products')
+    data = cursor.fetchall()
+    
+    return render_template('index.html',username=username,img=img,data=data)
+
+@app.route('/adminhome')
+def adminhome():
+    con = dbConnection()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM products')
+    data = cursor.fetchall()
+    
+    return render_template('adminhome.html',data=data)
+
+
+@app.route('/fontawesome')
+def fontawesome():
+    username=session['user']
+    img=session['image']
+    return render_template('fontawesome.html',username=username,img=img)
+
+
+@app.route('/mapgoogle')
+def mapgoogle():
+    username=session['user']
+    img=session['image']
+    
+    return render_template('mapgoogle.html',username=username,img=img)
+
+@app.route('/mapgoogleadmin',methods=['POST','GET'])
+def mapgoogleadmin():
+    if request.method == "POST":
+        details = request.form
+        NAME = details['NAME']
+        PRICE = details['PRICE']
+        DESCRIPTION = details['DESCRIPTION']
+        TYPE= details['Type']
+        PCS = details['PCS']
+        uploadimg = request.files['file']
+        
+        con = dbConnection()
+        cursor = con.cursor()
+        filename_secure = secure_filename(uploadimg.filename)
+        uploadimg.save(os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename_secure))
+        filenamepath = os.path.join(app.config['UPLOADED_PHOTOS_DEST'], filename_secure)
+        
+        sql = "INSERT INTO products(NAME, PRICE, DESCRIPTION, TYPE, PCS, filenamepath) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (NAME, PRICE, DESCRIPTION, TYPE, PCS,filenamepath)
+        cursor.execute(sql, val)
+        con.commit()
+        
+        message = "Product Added successfully" 
+        print(message)
+        return render_template('mapgoogleadmin.html',message=message)
+        
+        
+        
+    
+    
+    
+    
+    return render_template('mapgoogleadmin.html')
+
+@app.route('/pagesblank')
+def pagesblank():
+    username=session['user']
+    img=session['image']
+    return render_template('pagesblank.html',username=username,img=img)
+
+
+@app.route('/pages')
+def pages():
+    username=session['user']
+    img=session['image']
+   
+    return render_template('pages.html',username=username,img=img)
+
+
+@app.route('/profile')
+def profile():
+    username=session['user']
+    img=session['image']
+    con = dbConnection()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM userregisters WHERE Username = %s and Profile_Img = %s', (username,img))
+    data = cursor.fetchone()
+    print(data)
+ 
+    
+    return render_template('profile.html',username=username,img=img,data=data)
+
+
+@app.route('/tablebasic')
+def tablebasic():
+    username=session['user']
+    img=session['image']
+    
+    
+    return render_template('tablebasic.html',username=username,img=img)
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('main'))
+
+@app.route('/logout1')
+def logout1():
+   
+    return redirect(url_for('main'))
+
+@app.route('/viewpro/<productId>')
+def viewpro(productId):
+    username=session['user']
+    img=session['image']
+    print(productId)
+
+    con = dbConnection()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM products WHERE PRODUCT_ID = %s ', (productId))
+    data = cursor.fetchone()
+    # print(data)
+    row=list(data)
+    print(row)
+    cursor.execute('SELECT * FROM recomdedpro WHERE productid = %s ', (productId))
+    reviews = cursor.fetchall()
+    
+    return render_template('viewpro.html',username=username,img=img,row=row,reviews=reviews)
+
+####################################################################feedback users############################################################
+@app.route('/Review', methods=["POST"])
+def Review():
+    print("GET")
+    USERID=session['uid']
+    username=session['user']
+    img=session['image']
+    
+    
+    if request.method =='POST':
+        print("Post")
+        rating = request.form.get('rating')
+        comment = request.form.get('comment')
+        productname = request.form.get('productname')
+        productid = request.form.get('productid')
+        producttype = request.form.get('producttype')
+       
+        con = dbConnection()
+        cursor = con.cursor()
+        sql = "INSERT INTO Recomdedpro(productname, productid, comment, username, username_img, USERID ,producttype,rating) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (productname, productid, comment, username, img, USERID ,producttype, rating)
+        cursor.execute(sql, val)
+        print("Query submitted...")
+        con.commit()
+        
+        msg = "Review Submited Successfuly....." 
+        return msg
+            
+      
+#################################################recommended business###############################################################################
+@app.route('/viewproadmin/<productId>')
+def viewproadmin(productId):
+   
+    con = dbConnection()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM products WHERE PRODUCT_ID = %s ', (productId))
+    data = cursor.fetchone()
+    # print(data)
+    row=list(data)
+    print(row)
+    cursor.execute('SELECT * FROM recomdedpro WHERE productid = %s ', (productId))
+    reviews = cursor.fetchall()
+    
+    return render_template('viewproadmin.html',row=row,reviews=reviews)
+
+################################################################################################################################
+@app.route('/upload',methods=['POST','GET'])
+def upload():
+    USERID=session['uid']
+    if request.method == "POST":
+        print("===============================================")
+        file = request.files['file']
+        print(file)
+           
+         
+        filename = secure_filename(file.filename)
+        print(filename)
+        file.save(os.path.join(app.config['UPLOADED_PHOTOS_DEST2'], filename))
+        img = cv2.imread("static/skindetecttype/"+str(filename))
+           
+         
+        image_size=224
+        #img = cv2.imread(path1+"//"+i)
+        path="static/skindetecttype/"+"//"+str(filename)
+        img = image.load_img(path, target_size=(image_size, image_size))
+        x = image.img_to_array(img)
+        print(type(x))
+        img_4d=x.reshape(1,224,224,3)
+          
+        predictions = model.predict(img_4d)
+        print(predictions)
+        pred=np.argmax(predictions[0])
+        print("===============================================")
+        print(pred)
+        print("===============================================")
+        dict1 = {0:'Combination',1:'Dry',2:'Normal',3:'Oily'}
+        op=dict1[pred]
+        print(op)
+           
+        if op == "Combination":
+             final_prediction = "Combination"
+        elif op == "Dry":
+             final_prediction = "Dry"
+        elif op == "Oily":
+             final_prediction = "Oily"
+        elif op == "Normal":
+             final_prediction = "Normal"
+        else:
+             final_prediction = "Unknown"
+        
+        message = "Cosmetics suggestion System based on Skin Condition" +" "+ final_prediction
+        
+        print(message)
+        
+        rec_lst = []
+        try:
+            rec_lst = recommend(int(USERID))
+            print("in")
+            print(rec_lst)
+        except:
+            print("outy")
+            rec_lst = [1,2]
+            print(rec_lst)
+          
+        mainrowlst = []
+        for k in rec_lst:
+            cursor.execute('SELECT * FROM products WHERE PRODUCT_ID = %s;',(str(k)))
+            row = cursor.fetchall()
+            print(row)
+            mainrowlst.append(row)
+            
+        
+        jsonObj = json.dumps([mainrowlst,message,path])
+           
+        return jsonObj 
+  
+    return jsonObj
+       
+      
+       
+    #    cursor.execute('SELECT * FROM products where TYPE= %s;',(str(final_prediction)))
+    #    row = cursor.fetchall()
+    #     # print(row)
+    #     # jsonObj = json.dumps(row)
+    #    jsonObj = json.dumps([row,message,path])
+       
+    #    print("jsonObj")
+    #    print(jsonObj)
+    #    return jsonObj
+     
+    # return jsonObj
+
+
+#################################################recommended product###############################################################################
+def recommend(USERID):
+    conn = dbConnection()
+    cur = conn.cursor()
+    sql="SELECT USERID,productid,rating from recomdedpro"
+    cur.execute(sql)
+    table_rows = cur.fetchall()
+    print(table_rows)
+    df = pd.DataFrame(table_rows,columns=['USERID','productid','rating'])
+    #df.to_csv("Reco.csv")
+    print()
+    print("printign np.unique(df['productid']")
+    print(np.unique(df['productid']))
+    a=len(np.unique(df['productid']))
+
+    print()
+    print("printing a")
+    print(a)
+    print()
+    df = df.astype({"USERID": int,"productid": int,"rating": int })
+    ratings_utility_matrix=df.pivot_table(values='rating', index='USERID', columns='productid', fill_value=0)
+    X = ratings_utility_matrix.T
+    print("---------------------------")
+    print(X)
+    print("--------------------")
+    import sklearn
+    from sklearn.decomposition import TruncatedSVD
+    SVD = TruncatedSVD(n_components=int(2))
+    decomposed_matrix = SVD.fit_transform(X)
+    correlation_matrix = np.corrcoef(decomposed_matrix)
+    print("==========")
+    print(correlation_matrix)
+    i = USERID
+    
+    product_names = list(X.index)
+    print('----------------product_names-------------------')
+    print(product_names)
+    print(i)
+    print('-----------------------------------')
+    product_ID = product_names[i]
+    correlation_product_ID = correlation_matrix[product_ID]
+    Recommend = list(X.index[correlation_product_ID > 0.70])
+#     Recommend.remove(i) 
+    print("complete")
+    
+    return Recommend[0:3]
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run('0.0.0.0')
---
+++ Finalcode/project/finaldb.sql
@@ -0,0 +1,79 @@
+/*
+SQLyog Community Edition- MySQL GUI v7.01 
+MySQL - 5.0.27-community-nt : Database - 032-cosmetics
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`032-cosmetics` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `032-cosmetics`;
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `PRODUCT_ID` int(255) NOT NULL auto_increment,
+  `NAME` varchar(255) default NULL,
+  `PRICE` varchar(255) default NULL,
+  `DESCRIPTION` longtext,
+  `TYPE` varchar(255) default NULL,
+  `PCS` varchar(255) default NULL,
+  `filenamepath` varchar(255) default NULL,
+  PRIMARY KEY  (`PRODUCT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `products` */
+
+insert  into `products`(`PRODUCT_ID`,`NAME`,`PRICE`,`DESCRIPTION`,`TYPE`,`PCS`,`filenamepath`) values (1,'skin lacto','2000','Re\'equil Ceramide & Hyaluronic Acid Moisturiser For Normal To Dry Skin','Combination','6','static/profile/download.jpg'),(2,'Lakme ','9999','  Best Lakme Products','Combination','5','static/profile/as.jpg'),(3,'Tresemme Smooth & Shine','1500','Tresemme Smooth & Shine Pro Collection Shampoo - Vitamin H & Silk Protein, 580 ml Bottle','Dry','3','static/profile/images_1.jpg'),(4,'Legal frame','6000','A cosmetic product is defined in the Regulation as any substance ','Oily','3','static/profile/ff.jpg'),(5,'Stainless Steel Herbal Cosmetic','9000','Stainless Steel Herbal Cosmetic Products at Best Price in Pune','Combination','4','static/profile/f.jpg'),(6,'Love Beauty','10000','Love Beauty & Planet Natural Murumuru Butter and Rose Sulfate Free Body Wash','Normal','4','static/profile/d.jpg');
+
+/*Table structure for table `recomdedpro` */
+
+DROP TABLE IF EXISTS `recomdedpro`;
+
+CREATE TABLE `recomdedpro` (
+  `id` int(255) NOT NULL auto_increment,
+  `productname` varchar(255) default NULL,
+  `productid` varchar(255) default NULL,
+  `comment` longtext,
+  `username` varchar(255) default NULL,
+  `username_img` varchar(255) default NULL,
+  `USERID` varchar(255) default NULL,
+  `producttype` varchar(255) default NULL,
+  `rating` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `recomdedpro` */
+
+insert  into `recomdedpro`(`id`,`productname`,`productid`,`comment`,`username`,`username_img`,`USERID`,`producttype`,`rating`) values (1,'skin lacto','1','not good but also bad','roshan','static/profile/client_1.jpg','1','Combination','4'),(2,'Lakme ','2','this was good product','roshan','static/profile/client_1.jpg','1','Combination','2'),(3,'Tresemme Smooth & Shine','3','not good but also bad','roshan','static/profile/client_1.jpg','1','Dry','5'),(4,'Legal frame','4','not good but also bad','roshan','static/profile/client_1.jpg','1','Oily','4'),(5,'skin lacto','1','this was good product','yash','static/profile/client_2.jpg','2','Combination','3'),(6,'Lakme ','2','this was good product','yash','static/profile/client_2.jpg','2','Combination','5'),(7,'Stainless Steel Herbal Cosmetic','5','this was good product','yash','static/profile/client_2.jpg','2','Combination','3'),(8,'Tresemme Smooth & Shine','3','This paper focuses on predicting the yield of the crop by applying various machine learning techniques.','yash','static/profile/client_2.jpg','2','Dry','4'),(9,'Love Beauty','6','not good but also bad','yash','static/profile/client_2.jpg','2','Normal','5');
+
+/*Table structure for table `userregisters` */
+
+DROP TABLE IF EXISTS `userregisters`;
+
+CREATE TABLE `userregisters` (
+  `Id` int(255) NOT NULL auto_increment,
+  `Username` varchar(255) default NULL,
+  `Email` varchar(255) default NULL,
+  `Mobile` varchar(255) default NULL,
+  `Password` varchar(255) default NULL,
+  `Profile_Img` varchar(255) default NULL,
+  `Address` varchar(255) default NULL,
+  `Pancard` varchar(255) default NULL,
+  PRIMARY KEY  (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `userregisters` */
+
+insert  into `userregisters`(`Id`,`Username`,`Email`,`Mobile`,`Password`,`Profile_Img`,`Address`,`Pancard`) values (1,'roshan','sujay@gmail.com','9561161391','Abc@123','static/profile/client_1.jpg','mumbai','456123123123'),(2,'yash','stawar59@gmail.com','8796655332','Yash@123','static/profile/client_2.jpg','vikroli','963258741147');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
